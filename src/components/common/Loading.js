1195b71648cc21cae377bf448e94cb8d145a48d6
kvn-media <kevin.subagio@gmail.com>
2024-01-21 17:15:53 +0700

    add loading component

---
+++ src/components/common/Loading.js
@@ -0,0 +1,13 @@
+// src/components/common/Loading.js
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="Container text-center p-4">
+      <p>Loading...</p>
+      {/* Add a loading spinner or animation if needed */}
+    </div>
+  );
+};
+
+export default Loading;
